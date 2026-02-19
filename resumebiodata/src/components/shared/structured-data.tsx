c3ef4d8573efda5a9aa6105f512f9efa6b4fb9b3
rathodshreyas <maulimultiservices.7@gmail.com>
2026-02-19 19:07:52 +0530

    Delete resumebiodata directory

--- resumebiodata/src/components/shared/structured-data.tsx
+++
@@ -1,8 +0,0 @@
-export default function StructuredData({ data }: { data: object }) {
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-    />
-  );
-}
